b498aa215a920d4e30e259bcc47f63d783416b5a
Angela Battillo <angela.battillo@gmail.com>
2020-12-12 16:36:37 -0700

    Add second button

--- src/main.js
+++ src/main.js
@@ -1,19 +1,62 @@
-// Create variables targetting the relevant DOM elements here 👇
 
 
-// We've provided a few variables below
+//Query Selectors
+var randomCoverButton = document.querySelector('.random-cover-button');
+var coverImage = document.querySelector('.cover-image');
+var coverTitle = document.querySelector('.cover-title');
+var taglineOne = document.querySelector('.tagline-1');
+var taglineTwo = document.querySelector('.tagline-2');
+
+var makeYourOwnCoverButton = document.querySelector('.make-new-button');
+
+var formView = document.querySelector('.form-view');
+var homeView = document.querySelector('.home-view');
+var savedView = document.querySelector('.saved-view');
+var viewSavedCovers = document.querySelector('.view-saved-button');
+
+
+
+// Global Variables
 var savedCovers = [
   new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
 ];
 var currentCover;
 
-// Add your event listeners here 👇
+// Event Listeners
+
+randomCoverButton.addEventListener('click', makeNewRandomCover);
+window.addEventListener('load', makeNewRandomCover);
+makeYourOwnCoverButton.addEventListener('click', goToInputForm)
+viewSavedCovers.addEventListener('click', goToSavedCovers)
 
 
-// Create your event handlers and other functions here 👇
+// Functions
+function goToInputForm() {
+  formView.classList.remove('hidden')
+  homeView.classList.add('hidden')
+}
+
+function goToSavedCovers() {
+  savedView.classList.remove('hidden')
+  formView.classList.add('hidden')
+}
+
+function makeNewRandomCover(){
+  event.preventDefault();
+var randomCover = covers[getRandomIndex(covers)];
+var randomTitle = titles[getRandomIndex(titles)];
+var randomDescriptor1 = descriptors[getRandomIndex(descriptors)];
+var randomDescriptor2 = descriptors[getRandomIndex(descriptors)];
+
+currentCover = new Cover(randomCover, randomTitle, randomDescriptor1, randomDescriptor2)
+
+coverImage.src = randomCover;
+coverTitle.innerHTML = randomTitle;
+taglineOne.innerHTML = randomDescriptor1;
+taglineTwo.innerHTML = randomDescriptor2;
+}
 
 
-// We've provided one function to get you started
 function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length);
 }
